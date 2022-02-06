1d2f781aae7036d2006379de5545b6b7a64ffe60
RohithSen1235 <rohithsen2011@gmail.com>
2022-02-07 02:31:14 +0530

    Cleaned the code, used loops to addEventListeners

--- DOM/GameProject/TicTacToe.js
+++ DOM/GameProject/TicTacToe.js
@@ -1,147 +1,36 @@
-var box1 = document.querySelector("#one");
-var box2 = document.querySelector("#two");
-var box3 = document.querySelector("#three");
-var box4 = document.querySelector("#four");
-var box5 = document.querySelector("#five");
-var box6 = document.querySelector("#six");
-var box7 = document.querySelector("#seven");
-var box8 = document.querySelector("#eight");
-var box9 = document.querySelector("#nine");
+// Restart functionality
+var restart=document.querySelector("#restart");
 
 
+// Selecting all the boxes
 
-box1.addEventListener('click', async function(){   
-    
-    box1.innerHTML="<strong>X</strong>";
-    box1.style.color= "blue";
+var squares=document.querySelectorAll(".square")
 
-});
+// function for clearing the board
+function clearBoard(){
+    for(var sq in squares)
+    {
+        squares[sq].textContent="";
+    }
+}
 
-box1.addEventListener('dblclick',async function(){   
-    
-    box1.innerHTML="<strong>O</strong>";
-    box1.style.color= "orange";
+// function for changing the marker in a given field
+function changeMarker(){
+    if(this.textContent===""){
+        this.textContent ="X";
+        this.style.color="blue";
+    }
+    else if(this.textContent==="X"){
+        this.textContent="O";
+        this.style.color= "orange";
+    }
+    else{
+        this.textContent="";
+    }
+}
 
-});
+restart.addEventListener('click',clearBoard);
 
-box2.addEventListener('click', async function(){   
-    
-    box2.innerHTML="<strong>X</strong>";
-    box2.style.color= "blue";
-
-});
-
-box2.addEventListener('dblclick',async function(){   
-    
-    box2.innerHTML="<strong>O</strong>";
-    box2.style.color= "orange";
-
-});
-
-box3.addEventListener('click', async function(){   
-    
-    box3.innerHTML="<strong>X</strong>";
-    box3.style.color= "blue";
-
-});
-
-box3.addEventListener('dblclick',async function(){   
-    
-    box3.innerHTML="<strong>O</strong>";
-    box3.style.color= "orange";
-
-});
-
-// 4
-box4.addEventListener('click', async function(){   
-    
-    box4.innerHTML="<strong>X</strong>";
-    box4.style.color= "blue";
-
-});
-
-box4.addEventListener('dblclick',async function(){   
-    
-    box4.innerHTML="<strong>O</strong>";
-    box4.style.color= "orange";
-
-});
-
-// 5
-box5.addEventListener('click', async function(){   
-    
-    box5.innerHTML="<strong>X</strong>";
-    box5.style.color= "blue";
-
-});
-
-box5.addEventListener('dblclick',async function(){   
-    
-    box5.innerHTML="<strong>O</strong>";
-    box5.style.color= "orange";
-
-});
-
-// 6
-box6.addEventListener('click', async function(){   
-    
-    box6.innerHTML="<strong>X</strong>";
-    box6.style.color= "blue";
-
-});
-
-box6.addEventListener('dblclick',async function(){   
-    
-    box6.innerHTML="<strong>O</strong>";
-    box6.style.color= "orange";
-
-});
-
-// 7
-box7.addEventListener('click', async function(){   
-    
-    box7.innerHTML="<strong>X</strong>";
-    box7.style.color= "blue";
-
-});
-
-box7.addEventListener('dblclick',async function(){   
-    
-    box7.innerHTML="<strong>O</strong>";
-    box7.style.color= "orange";
-
-});
-
-// 8
-box8.addEventListener('click', async function(){   
-    
-    box8.innerHTML="<strong>X</strong>";
-    box8.style.color= "blue";
-
-});
-
-box8.addEventListener('dblclick',async function(){   
-    
-    box8.innerHTML="<strong>O</strong>";
-    box8.style.color= "orange";
-
-});
-
-// 9
-box9.addEventListener('click', async function(){   
-    
-    box9.innerHTML="<strong>X</strong>";
-    box9.style.color= "blue";
-
-});
-
-box9.addEventListener('dblclick',async function(){   
-    
-    box9.innerHTML="<strong>O</strong>";
-    box9.style.color= "orange";
-
-});
-
-
-
-
+for(var sq in squares){
+    squares[sq].addEventListener('click',changeMarker);
+}
